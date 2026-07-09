8090ebf75a075e4636a2a9ab4b47e6d32fbcd01b
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-07-09 10:08:00 +0530

    update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,2 @@
-# Changelog
-
-## 0.0.2
-  * Bump requests to 2.33.0 for security updates [#4](https://github.com/singer-io/tap-oracle-fusion/pull/4)
+## 0.0.1
+  * Initial Commit
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-oracle-fusion",
-    version="0.0.2",
+    version="0.0.1",
     description="Singer.io tap for extracting data from oracle-fusion API",
     author="Stitch",
     url="http://singer.io",
